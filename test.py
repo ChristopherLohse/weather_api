694176868b6786c5ee99d77aa058fea1e87d1dff
christopher-lohse <christopher.lohse@ibm.com>
2021-03-13 15:10:06 +0100

    delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-files = os.listdir(".")
-for index, file in enumerate(files):
-    if os.path.getsize(file) == 0:
-        continue
-    if index < training_stop:
-        copyfile(file, TRAINING+"/"+file)
-    else:
-        copyfile(file, TEST+"/"+file)
